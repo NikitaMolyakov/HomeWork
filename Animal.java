aa48c1ad7321e0fff72445445d09c9e17056b840
Никита Моляков <126617001+NikitaMolyakov@users.noreply.github.com>
2025-01-11 00:33:02 +0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+    private static int count = 0;
+
+    public Animal() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void run(int distance) {
+    }
+
+    public void swim(int distance){
+    }
+}
+
