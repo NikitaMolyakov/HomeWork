ac96446c103ff7775dcb95fdb1589a03d4d7391e
Никита Моляков <126617001+NikitaMolyakov@users.noreply.github.com>
2025-01-11 00:33:14 +0300

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,26 @@
+public class Dog extends Animal {
+    private static final int MAX_RUN_DISTANCE = 500;
+    private static final int MAX_SWIM_DISTANCE = 10;
+
+    public Dog() {
+        super();
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= MAX_RUN_DISTANCE) {
+            System.out.println("Собака пробежала " + distance + " м.");
+        } else {
+            System.out.println("Собака не может пробежать больше " + MAX_RUN_DISTANCE + " м.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= MAX_SWIM_DISTANCE) {
+            System.out.println("Собака проплыла " + distance + " м.");
+        } else {
+            System.out.println("Собака не может проплыть больше " + MAX_SWIM_DISTANCE + " м.");
+        }
+    }
+}
